e1463e59be4c41b82e9d3479a9b4345fb4a02b44
MulaAshish <C0837185@mylambton.ca>
2022-03-14 23:24:52 -0400

    Initial Commit

---
+++ TheEternals_Capstone_Projet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheEternals_Capstone_Projet
+//
+//  Created by Ashish reddy mula on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
