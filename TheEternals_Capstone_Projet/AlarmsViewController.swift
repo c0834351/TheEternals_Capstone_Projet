c1e2c6b5d06323d44e73bac4ff90ea64e568da35
Sravan <c0828149@mylambton.ca>
2022-03-20 13:16:21 -0400

    Added searchbar and some properties to it

--- TheEternals_Capstone_Projet/AlarmsViewController.swift
+++ TheEternals_Capstone_Projet/AlarmsViewController.swift
@@ -9,14 +9,19 @@ import UIKit
 
 class AlarmsViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        searchBar.backgroundImage = UIImage()
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [UIColor.systemBlue.cgColor, UIColor.systemGray3.cgColor]
 
         view.layer.insertSublayer(gradientLayer, at: 0)
+        
     }
 
 }
