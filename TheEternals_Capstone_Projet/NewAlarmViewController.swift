6debc25aef0720c4600ed6689c67d67d756c85cb
c0834351 <c0834351@mylambton.ca>
2022-03-21 14:25:30 -0400

    connected-buttons-switch-elements-in-createAlarmScreen

--- TheEternals_Capstone_Projet/NewAlarmViewController.swift
+++ TheEternals_Capstone_Projet/NewAlarmViewController.swift
@@ -10,22 +10,38 @@ import UIKit
 class NewAlarmViewController: UIViewController {
     
     @IBOutlet weak var datesVStackview: UIStackView!
-    
     @IBOutlet weak var weekdaysStackView: UIStackView!
-    
     @IBOutlet weak var alarmToneVStackView: UIStackView!
-    
     @IBOutlet weak var picturesHStackView: UIStackView!
-    
     @IBOutlet weak var repeatStackView: UIStackView!
+    
     @IBOutlet weak var weekdaysSVConstraint: NSLayoutConstraint!
     @IBOutlet weak var alarmToneVSHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var picturesHSHeightConstraint: NSLayoutConstraint!
+    
     @IBOutlet weak var alarmTitle: UITextField!
     
     @IBOutlet weak var image1: UIImageView!
     @IBOutlet weak var image2: UIImageView!
     @IBOutlet weak var image3: UIImageView!
+    
+    @IBOutlet weak var snoozeSwitch: UISwitch!
+    @IBOutlet weak var repeatFlag: UISwitch!
+    
+    @IBOutlet weak var sundayButton: UIButton!
+    @IBOutlet weak var mondayButton: UIButton!
+    @IBOutlet weak var TuesdayButton: UIButton!
+    @IBOutlet weak var wednesdayButton: UIButton!
+    @IBOutlet weak var thursdayButton: UIButton!
+    @IBOutlet weak var fridayButton: UIButton!
+    @IBOutlet weak var saturdayButton: UIButton!
+    
+    
+    
+    @IBOutlet weak var alarmTime: UIDatePicker!
+    @IBOutlet weak var startDate: UIDatePicker!
+    @IBOutlet weak var endDate: UIDatePicker!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
