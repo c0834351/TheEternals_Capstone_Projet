9b85759cfdb61b124d4cbba60b9308cb5a2f0b18
c0834351 <c0834351@mylambton.ca>
2022-03-21 14:35:00 -0400

    Connecting-elements-in-createAlarmScreen

--- TheEternals_Capstone_Projet/NewAlarmViewController.swift
+++ TheEternals_Capstone_Projet/NewAlarmViewController.swift
@@ -9,25 +9,31 @@ import UIKit
 
 class NewAlarmViewController: UIViewController {
     
+    //StackViews
     @IBOutlet weak var datesVStackview: UIStackView!
     @IBOutlet weak var weekdaysStackView: UIStackView!
     @IBOutlet weak var alarmToneVStackView: UIStackView!
     @IBOutlet weak var picturesHStackView: UIStackView!
     @IBOutlet weak var repeatStackView: UIStackView!
     
+    //Constraints
     @IBOutlet weak var weekdaysSVConstraint: NSLayoutConstraint!
     @IBOutlet weak var alarmToneVSHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var picturesHSHeightConstraint: NSLayoutConstraint!
     
+    //TextFields
     @IBOutlet weak var alarmTitle: UITextField!
     
+    //ImageViews
     @IBOutlet weak var image1: UIImageView!
     @IBOutlet weak var image2: UIImageView!
     @IBOutlet weak var image3: UIImageView!
     
+    //Switches
     @IBOutlet weak var snoozeSwitch: UISwitch!
     @IBOutlet weak var repeatFlag: UISwitch!
     
+    //Buttons
     @IBOutlet weak var sundayButton: UIButton!
     @IBOutlet weak var mondayButton: UIButton!
     @IBOutlet weak var TuesdayButton: UIButton!
@@ -35,9 +41,13 @@ class NewAlarmViewController: UIViewController {
     @IBOutlet weak var thursdayButton: UIButton!
     @IBOutlet weak var fridayButton: UIButton!
     @IBOutlet weak var saturdayButton: UIButton!
+    @IBOutlet weak var recordBtnLB: UIButton!
+    @IBOutlet weak var playBtnLB: UIButton!
+    @IBOutlet weak var setDefaultToneBtnLB: UIButton!
+    @IBOutlet weak var showAudioOptionsBTN: UIButton!
+    @IBOutlet weak var showPicturesOptionsBTN: UIButton!
     
-    
-    
+    //DatePickers
     @IBOutlet weak var alarmTime: UIDatePicker!
     @IBOutlet weak var startDate: UIDatePicker!
     @IBOutlet weak var endDate: UIDatePicker!
@@ -53,7 +63,9 @@ class NewAlarmViewController: UIViewController {
         
     }
     
-
+    @IBAction func snoozeSwitchTapped(_ sender: Any) {
+    }
+    
     
 
 }
