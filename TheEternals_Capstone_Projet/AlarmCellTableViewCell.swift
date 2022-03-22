bed5786594b2cf5ccbe5991481f4cae82790970e
pavan3730 <90772828+pavan3730@users.noreply.github.com>
2022-03-21 20:48:34 -0400

    Added tableview to second screen

---
+++ TheEternals_Capstone_Projet/AlarmCellTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  AlarmCellTableViewCell.swift
+//  TheEternals_Capstone_Projet
+//
+//  Created by Keerthi Pavan Valluri on 2022-03-21.
+//
+
+import UIKit
+
+class AlarmCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cardView: UIView!
+    
+    @IBOutlet weak var afterFood: UILabel!
+    @IBOutlet weak var enabled: UISwitch!
+    @IBOutlet weak var medicines: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var medimage: UIImageView!
+    
+    func setCell(picture: UIImage, timeValue: String, afterFoodValue: String, medicinesValue: String, enabledValue: Bool){
+        medimage.image = picture
+        time.text = timeValue
+        afterFood.text = afterFoodValue
+        medicines.text = medicinesValue
+        enabled.setOn(enabledValue, animated: true)
+        
+        cardView.layer.shadowColor = UIColor.clear.cgColor
+        cardView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        cardView.layer.cornerRadius = 14
+        cardView.layer.shadowOpacity = 1.0
+        cardView.layer.masksToBounds = false
+    }
+    
+}
--- TheEternals_Capstone_Projet/AlarmsViewController.swift
+++ TheEternals_Capstone_Projet/AlarmsViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import CoreData
 
 class AlarmsViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
     
+
+    @IBOutlet weak var allAlarmsTV: UITableView!
+    var cellimage:UIImage!
     
+    private var allalarms = [Alarm]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +28,29 @@ class AlarmsViewController: UIViewController {
 
         view.layer.insertSublayer(gradientLayer, at: 0)
         
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allalarms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "alarmcell", for: indexPath) as! AlarmCellTableViewCell
+        if let imagedata = allalarms[indexPath.row].value(forKey: "image1") as? Data{
+            cellimage = UIImage(data: imagedata)
+        }
+        
+        cell.setCell(picture: cellimage, timeValue: allalarms[indexPath.row].time ?? "", afterFoodValue: allalarms[indexPath.row].whentotake ?? "", medicinesValue: allalarms[indexPath.row].title ?? "", enabledValue: allalarms[indexPath.row].enabled)
+        return cell
+    }
+    
+    private func getAlarms(){
+        let request:NSFetchRequest<Alarm> = Alarm.fetchRequest()
+        do {
+            self.allalarms = try context.fetch(request)
+        } catch {
+            print("Error load items ... \(error.localizedDescription)")
+        }
     }
 
 }
