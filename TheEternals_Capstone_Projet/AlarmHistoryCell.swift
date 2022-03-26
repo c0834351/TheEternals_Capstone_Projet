54581d4220f3a32f07cb70e5ff8dcc661295f5b0
pavan3730 <90772828+pavan3730@users.noreply.github.com>
2022-03-26 17:28:40 -0400

    Added history screen changes and some UI elements

---
+++ TheEternals_Capstone_Projet/AlarmHistoryCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlarmHistoryCell.swift
+//  TheEternals_Capstone_Projet
+//
+//  Created by Keerthi Pavan Valluri on 2022-03-26.
+//
+
+import UIKit
+
+class AlarmHistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TheEternals_Capstone_Projet/HistoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryViewController.swift
+//  TheEternals_Capstone_Projet
+//
+//  Created by Keerthi Pavan Valluri on 2022-03-26.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
