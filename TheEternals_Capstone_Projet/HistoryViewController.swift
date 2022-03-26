c848909e9cf00bde4e62d96c81f228a76c763118
pavan3730 <90772828+pavan3730@users.noreply.github.com>
2022-03-26 17:37:53 -0400

    Added history screen changes

--- TheEternals_Capstone_Projet/HistoryViewController.swift
+++ TheEternals_Capstone_Projet/HistoryViewController.swift
@@ -13,11 +13,35 @@ class HistoryViewController: UIViewController {
     
     @IBOutlet weak var sortbyBTN: UIButton!
     
+    var alarmhistory = [History]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "History"
         // Do any additional setup after loading the view.
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.systemBlue.cgColor, UIColor.systemGray3.cgColor]
+
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
 
+}
+
+
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (alarmhistory.count == 0){
+            self.historyTV.setEmptyMessage("")
+        } else {
+            historyTV.restore()
+        }
+        return alarmhistory.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "historycell", for: indexPath) as! AlarmHistoryCell
+        
+        return cell
+    }
 }
